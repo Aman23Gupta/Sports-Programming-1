6582c4e99ac7feb274a765c7429678cc5c5b7bf4
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2019-12-08 22:32:21 +0530

    Update and rename ceil_floor.cpp to builtin_func.cpp

---
+++ builtin_func.cpp
@@ -0,0 +1,19 @@
+/*
+
+ceil is SIF smallest integer greater than given float;
+rounding up
+ceil(a/b)=(a+b-1)/b;
+
+rounding down
+floor is GIF greatest integer smaller than given float;
+floor(a/b)=ceil(a/b)-1=((a+b-1)/b)-1;
+
+
+
+lower_bound points to first element greater than or equal to val.
+upper_bound points to first element greater than val
+
+usage
+lower = lower_bound (v.begin(), v.end(), val);  
+upper = upper_bound (v.begin(), v.end(), val);  
+*/
--- ceil_floor.cpp
+++
@@ -1,4 +0,0 @@
-/*
-ceil(a/b)=(a+b-1)/b;
-floor(a/b)=ceil(a/b)-1=((a+b-1)/b)-1;
-*/
