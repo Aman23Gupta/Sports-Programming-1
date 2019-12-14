fc26812d585ebe81cad5b81ba92f4e68543af0d1
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2019-12-14 11:59:56 +0530

    Update builtin_func.cpp

--- builtin_func.cpp
+++ builtin_func.cpp
@@ -1,9 +1,8 @@
 /*
 
-ceil is SIF smallest integer greater than given float;
 rounding up
+ceil is SIF smallest integer greater than given float;
 ceil(a/b)=(a+b-1)/b;
-
 rounding down
 floor is GIF greatest integer smaller than given float;
 floor(a/b)=ceil(a/b)-1=((a+b-1)/b)-1;
@@ -12,8 +11,20 @@ floor(a/b)=ceil(a/b)-1=((a+b-1)/b)-1;
 
 lower_bound points to first element greater than or equal to val.
 upper_bound points to first element greater than val
-
 usage
 lower = lower_bound (v.begin(), v.end(), val);  
 upper = upper_bound (v.begin(), v.end(), val);  
+
+
+
+replace function with strings...
+s1=string in which replacement has to be made;
+s2=string that replaces;
+start=index where replacement would began;
+span=how many indexes it would cover;
+usage
+s1.replace(start,span,s2);
+
+
+
 */
