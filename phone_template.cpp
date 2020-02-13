1be5064509eae9e2eccf2c66874a1509c71d44cf
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-02-13 09:54:27 +0530

    Create phone_template.cpp

---
+++ phone_template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define pb          push_back
+#define all(a)      (a).begin(),(a).end()
+#define rall(a)      (a).rbegin(),(a).rend()
+#define endl        '\n'
+#define mod        1000000007
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+#define rem(i,a,b)  for(int i=a;i>b;i--)
+
+
+
+void solve(){
+
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    long t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
