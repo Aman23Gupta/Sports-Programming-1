e53b4b5e1b823aecfe45181b9b7cdd5ad53d03e4
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-02-27 01:47:51 +0530

    Update palindrome_generator.cpp

--- palindrome_generator.cpp
+++ palindrome_generator.cpp
@@ -1,1 +1,20 @@
+vector<ll> v;
 
+ll create(long n){
+    long temp=n;
+    if(temp%2==1)temp/10;
+    while(temp!=0){ 
+        n=n*10+temp%10;
+        temp/=10; 
+    }
+    return n;
+}
+
+void pal(ll limit){ 
+    ll num;
+    long i=1;
+    while((num=create(i))<limit){ 
+        v.pb(num);
+        i++;
+    }
+}
