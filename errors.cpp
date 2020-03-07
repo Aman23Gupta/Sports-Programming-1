7e10ce2d8dfe4d928c1aeff4cc37137dcb188af4
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-03-07 23:19:12 +0530

    Update errors.cpp

--- errors.cpp
+++ errors.cpp
@@ -1,1 +1,4 @@
+// when multipying two long numbers such that the result can be of longlong type then making the LHS equal to long long wouldn't be enough
+// individual operands on RHS should also be made long long
 
+// if there is no chance of working with negatives then you should use unsigned for extra range...
