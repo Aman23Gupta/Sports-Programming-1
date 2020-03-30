e2e55c7a16a912e718210ee33439bb599f932878
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-03-30 20:46:55 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,3 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define ull         unsigned long long
+#define pb          push_back
+#define pii         pair<int,int>
+#define pll         pair<long,long>
+#define vi          vector<int>
+#define vll         vector<ll>
+#define vii         vector<pii>
+#define Mi          map<int,int>
+#define mii         map<pii,int>
+#define all(a)      (a).begin(),(a).end()
+#define rall(a)      (a).rbegin(),(a).rend()
+#define ff           first
+#define ss           second
+#define sz(x)       (int)x.size()
+//#define endl        '\n'
+#define mod        1000000007
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+#define rem(i,a,b)  for(int i=a;i>b;i--)
+#define mp(a,b)     make_pair(a,b)
+#define INF         numeric_limits<ll>::max();
+#define NINF        numeric_limits<ll>::min();
+#define vvi(a,b,name)    vector<vector<int>> name(a,vector<int>(b,-1))
+
 struct node{
     node* par=nullptr;
     node* child[26]={nullptr};
@@ -67,4 +94,47 @@ void del(node* root,string s){
             }
         }
     }
+}
+
+void inorder(node* cur,string s){
+    if(cur->occ>0){
+        cout<<s<<" "<<cur->occ<<endl;
+    }
+    for(int i=0;i<26;i++){
+        if(cur->child[i]!=nullptr){
+            s+=(char)('a'+i);
+            inorder(cur->child[i],s);
+            s.pop_back();
+        }
+    }
+}
+
+void solve(){
+    node root;
+    string s;
+    rep(i,0,3){
+
+        cin>>s;
+        ins(&root,s);
+    }
+    inorder(&root,"");
+    cin>>s;
+    look(&root,s);
+    del(&root,s);
+    inorder(&root,"");
+
+
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    long t=1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
 }
