e6432a0852691c93bacea61f1c934c07d314e8a4
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-04-04 23:56:00 +0530

    Create perfecttemplate.cpp

---
+++ perfecttemplate.cpp
@@ -0,0 +1,18 @@
+/*------------------------------Graph Moves----------------------------*/
+//const int fx[]= {+1,-1,+0,+0};
+//const int fy[]= {+0,+0,+1,-1};
+//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*---------------------------------------------------------------------*/
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//
+//template <typename T>  using orderedSet =
+//    tree<T, null_type, less<T>,
+//    rb_tree_tag, tree_order_statistics_node_update>;//find_by_order,order_of_key
+//
