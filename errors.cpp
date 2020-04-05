800b2991ee58fb263fd4c6225d83a4360123d9b9
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-04-05 13:51:00 +0530

    Update errors.cpp

--- errors.cpp
+++ errors.cpp
@@ -2,3 +2,5 @@
 // individual operands on RHS should also be made long long
 
 // if there is no chance of working with negatives then you should use unsigned for extra range...
+
+//if question used one based indexing then you too, use one based indexing to avoid confusion.
