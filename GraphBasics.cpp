50da350e97cb2aae15271314f44518e345d1d7df
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-04-08 18:19:57 +0530

    Update GraphBasics.cpp

--- GraphBasics.cpp
+++ GraphBasics.cpp
@@ -1,10 +1,81 @@
 vector<int> v[10000];
 bool visited[10000];
 
+//basic/classic dfs
 void dfs(int node){
     visited[node]=true;
     for(auto child:v[node]){
         if(visited[child])continue;
         dfs(child);
+    }
+}
+
+//DFS can also be used to find SSSP in case of a tree... you require another array for storing distance and another arguement that
+//stores the distance to assigned right after we mark that node true... 
+
+//checking if given graph is Bipartite
+bool dfs(int node,int col){
+    visited[node]=true;
+    co[node]=col;
+    for(auto child:v[node]){
+        if(!visited[child]){
+            if(dfs(child,(col^1))==false)return false;
+        }
+        else{
+            if(co[node]==co[child])return false;
+        }
+    }
+    return true;
+}
+
+//cycle detection
+bool dfs(int node,int par){
+    visited[node]=true;
+    for(auto child:v[node]){
+        if(!visited[child]){
+            if(dfs(child,node))return true;
+        }
+        else{
+            if(child!=par)return true;
+        }
+    }
+    return false;
+}
+
+//inout helps in flattening of tree... after which MO's algorithm can be applied for processing queries on tree.
+//it can be used to efficiently solve queries given two node find if one occurs it other's subtree.
+//if n1 is in the subtree of p1 then in[p1]<in[n1] and out[n1]<out[p1]
+int in[1000];
+int out[1000];
+int timer=1;
+
+void dfs(int node){
+    visited[node]=true;
+    in[node]=timer++;
+    for(auto child:v[node]){
+        if(!visited[child])dfs(child);
+    }
+    out[node]=timer++;
+}
+
+
+// calculating diameter of a tree. It requires two run of DFS.
+// firstly you run a DFS from root node and find node farthest from root node.
+// then you run a DFS from that node... the max distance obtained is the diameter.
+
+//BFS
+void bfs(int node){
+    visited[node];
+    d[node]=0;
+    q.push(node);
+    while(!q.empty()){
+        int s=q.front();
+        q.pop();
+        for(auto u:v[s]){
+            if(visited[u])continue;
+            visited[u]=true;
+            d[u]=d[s]+1;
+            q.push(u);
+        }
     }
 }
