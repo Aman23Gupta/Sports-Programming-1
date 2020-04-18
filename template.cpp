0edad774b2809e4ef436ad49270347613fd9c2c4
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-04-18 13:48:35 +0530

    added tourist's debugger

--- template.cpp
+++ template.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+vector<string>vec_splitter(string s){s+=',';vector<string>res;while(!s.empty()){res.push_back(s.substr(0,s.find(',')));s=s.substr(s.find(',')+1);}
+return res;}
+void debug_out(vector<string>__attribute__((unused))args,__attribute__((unused))int idx,__attribute__((unused))int LINE_NUM){cerr<<endl;}
+template<typename Head,typename...Tail>void debug_out(vector<string>args,int idx,int LINE_NUM,Head H,Tail...T){if(idx>0)cerr<<", ";else cerr<<"Line("<<LINE_NUM<<") ";stringstream ss;ss<<H;cerr<<args[idx]<<" = "<<ss.str();debug_out(args,idx+1,LINE_NUM,T...);}
+#define XOX
+#ifdef XOX
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
 #define ll          long long
+#define ld          long double
 #define ull         unsigned long long
 #define pb          push_back
 #define pii         pair<int,int>
@@ -25,7 +35,7 @@ using namespace std;
 #define INF         numeric_limits<ll>::max();
 #define NINF        numeric_limits<ll>::min();
 #define vvi(a,b,name)    vector<vector<int>> name(a,vector<int>(b,-1))
-const ld pi=3.14159265359;
+//const ld pi=3.14159265359;
 inline ll add(ll a,ll b,ll m){
     if((a+b)>=m)return (a+b)%m;
     return a+b;
