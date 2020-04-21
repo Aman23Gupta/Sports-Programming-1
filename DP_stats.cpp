a0436214598a8f3e09fcaa0a831b29e30c69a8f9
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-04-21 13:29:52 +0530

    Update DP_stats.cpp

--- DP_stats.cpp
+++ DP_stats.cpp
@@ -1,2 +1,9 @@
 414B - Mashmokh and ACM
 dp[i][j] - the sequence of length i, that ends with number j.
+
+  
+  //resources for revising DP
+  // https://www.geeksforgeeks.org/dynamic-programming/
+  // https://blog.usejournal.com/top-50-dynamic-programming-practice-problems-4208fed71aa3
+  // https://atcoder.jp/contests/dp/tasks
+  // https://cses.fi/problemset/list/
