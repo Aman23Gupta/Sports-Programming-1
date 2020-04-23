da020183a7d4500bc1ac4e97b683ed67aedd3090
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-04-23 15:02:15 +0530

    Update and rename errors.cpp to Errors.cpp

--- Errors.cpp
+++ Errors.cpp
@@ -4,3 +4,10 @@
 // if there is no chance of working with negatives then you should use unsigned for extra range...
 
 //if question used one based indexing then you too, use one based indexing to avoid confusion.
+
+// whenever you use for(auto u:v) and u can be some container like vector, set, etc. use ampersand i.e. for(auto &u:v)
+
+// it is always efficient to declare global variable and clear them for every test case than redeclaring for each test case.
+
+// handling structures of int is faster than handling long long so if you are tight on time constraints and int would suffice then stick to
+// int
