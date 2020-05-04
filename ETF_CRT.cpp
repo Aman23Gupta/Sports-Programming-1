3831271910fdd38d8fdb0efa8d668e64cfc34002
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-05-04 20:23:02 +0530

    Update ETF_CRT.cpp

--- ETF_CRT.cpp
+++ ETF_CRT.cpp
@@ -4,6 +4,7 @@
 // phi(p^x)=p^x - all numbers from 1 tp p^x that are not co-prime with p^x;
 // we know that the answer to the second part would be all multiples of p, hence phi(p^x)=p^x-(p^x)/p=p^x-p^(x-1);
 
+// the sum of all the numbers less than 'n' and co-prime to it is n*(phi(n)/2);
 //CRT
 // We can use CRT to find 'X' such that we are given a list of pairwise co-primes p1,p2,p3...pn and the remainder that we obtain when X is
 // divided by  each of them as r1,r2,r3...rn.
