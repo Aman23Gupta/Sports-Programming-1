be3465c4087f69c787184c467368e10dde72b74a
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-05-13 13:22:04 +0530

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -2,3 +2,5 @@ https://codeforces.com/blog/entry/66850
 https://codeforces.com/blog/entry/18291
 https://codeforces.com/blog/entry/61587
 https://www.youtube.com/watch?v=JXTVOyQpSGM&t=843s
+https://codeforces.com/blog/entry/77304
+https://github.com/amit-nits/justforsharing/tree/master/Testing
