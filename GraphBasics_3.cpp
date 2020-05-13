2e31c0e3955229cb1ae88fb96ad09805228b9f43
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2020-05-13 18:01:09 +0530

    Update GraphBasics_3.cpp

--- GraphBasics_3.cpp
+++ GraphBasics_3.cpp
@@ -11,7 +11,7 @@ A path is a sequence of vertices with the property that each vertex in the seque
 A path that does not repeat vertices is called a simple path. 
 A circuit is path that begins and ends at the same vertex. A circuit that doesn't repeat vertices is called a cycle.
 An Euler path is a path that uses every edge of a graph exactly once.
-An Euler circuit is a circuit that uses every edge of a graph exactly once
+An Euler circuit/cycle is a circuit that uses every edge of a graph exactly once
 It appears that Euler Path is different from Euler circuit you'll call a sequence a Eluer path only when the point of start and end are different.
 
 criteria for existence of a Euler path
@@ -32,4 +32,59 @@ note that the aforementioned criteria's only gives you answers on the lines of "
 1, 3, 5,           No such graphs exist
 
 * provided the graph is connected..
+
+
+
+
+
+Necessary and sufficient conditions
+An undirected graph has a closed Euler tour iff it is connected and each vertex has an even degree.
+
+An undirected graph has an open Euler tour (Euler path) if it is connected, and each vertex, except for exactly two vertices, has an even degree.
+The two vertices of odd degree have to be the endpoints of the tour.
+
+A directed graph has a closed Euler tour iff it is strongly connected and the in-degree of each vertex is equal to its out-degree.
+
+Similarly, a directed graph has an open Euler tour (Euler path) iff for each vertex the difference between its in-degree and out-degree is 0, except for two vertices,
+where one has difference +1 (the start of the tour) and the other has difference -1 (the end of the tour) and, if you add an edge from the end to the start, 
+the graph is strongly connected.
 */
+
+//below is the implementation of Hierholzer's Algorithm for UNdirected graph
+//al in the adjacency list implemented as array of sets.
+//before calling check for aforementioned conditions.
+//if given graph meets the condition for Euler Ckt, then it can be called from any arbitrary vertex;
+//if give graph meets the condition for Euler Graph then it should be only called from odd degree vertices.
+//the final sequence of vertex can be obtained by rem(i,fsz-1,-1)cout<<final_path[i]<<" "; 
+    
+set<int> al[100000];
+int temp_path[400002], final_path[400005], tsz, fsz;
+ 
+void euler_dfs(int v){
+	temp_path[tsz++] = v;
+	while((int)al[v].size()!=0){
+		int vv=*al[v].begin();
+		al[v].erase(vv);
+		al[vv].erase(v);
+		euler_dfs(vv);
+	}
+	final_path[fsz++] = temp_path[--tsz];
+}
+
+//below is the implementation of Hierholzer's Algorithm for DIrected graph
+//adj refers to adjacency matrix of graph.
+//before calling check for aforementioned conditions.
+//if given graph meets the condition for Euler Ckt, then it can be called from any arbitrary vertex;
+//if give graph meets the condition for Euler Graph then it should be only called from vertex having outdeg>indeg.
+//the final sequence of vertex can be obtained by rem(i,fsz-1,-1)cout<<final_path[i]<<" "; 
+
+int temp_path[400002], final_path[400005], tsz, fsz;
+void euler_dfs(int v){
+	temp_path[tsz++] = v;
+	while(!adj[v].empty()){
+		int vv = adj[v].back();
+		adj[v].pop_back();
+		euler_dfs(vv);
+	}
+	final_path[fsz++] = temp_path[--tsz];
+}
