b5bfa4169df25dc3317ef0fee6f346712988f6c7
Aman Gupta <48331596+Aman23Gupta@users.noreply.github.com>
2021-08-02 17:42:58 +0530

    Changing to struct type

--- SegTree.cpp
+++ SegTree.cpp
@@ -1,40 +1,74 @@
-int a[200005];
-ll t[800005];
+struct segTree
+{
+    int size;
+    vll table;
+    void init(int n)
+    {
+        size = 1;
+        while (size < n)
+            size *= 2;
+        table.assign(2 * size, INF);
+    }
  
-void build(int v,int tl,int tr){
-    if(tl==tr){
-        t[v]=a[tl];
-    }
-    else{
-        int tm=(tl+tr)/2;
-        build(2*v,tl,tm);
-        build(2*v+1,tm+1,tr);
-        t[v]=min(t[2*v],t[2*v+1]);
-    }
-}
- 
-void update(int v,int tl,int tr,int pos,int val){
-    if(tl==tr){
-        t[v]=val;
-    }
-    else{
-        int tm=(tl+tr)/2;
-        if(tm>=pos){
-            update(2*v,tl,tm,pos,val);
+    void build(vll &a, int x, int lx, int rx)
+    {
+        if (rx - lx == 1)
+        {
+            if (lx < sz(a))
+            {
+                table[x] = a[lx];
+            }
+            return;
         }
-        else{
-            update(2*v+1,tm+1,tr,pos,val);
+        int m = (lx + rx) / 2;
+        build(a, 2 * x + 1, lx, m);
+        build(a, 2 * x + 2, m, rx);
+        table[x] = min(table[2 * x + 1],table[2 * x + 2]);
+    }
+ 
+    void build(vll &a)
+    {
+        build(a, 0, 0, size);
+    }
+ 
+    void set(int index, int value, int x, int lx, int rx)
+    {
+        if (rx - lx == 1)
+        {
+            table[x] = value;
+            return;
         }
-        t[v]=min(t[2*v],t[2*v+1]);
+        int m = (lx + rx) / 2;
+        if (index < m)
+        {
+            set(index, value, 2 * x + 1, lx, m);
+        }
+        else
+        {
+            set(index, value, 2 * x + 2, m, rx);
+        }
+        table[x] = min(table[2 * x + 1],table[2 * x + 2]);
     }
-}
  
-ll query(int v,int tl,int tr,int l,int r){
-    if(l>r)return INF;
-    if(tl==l && tr==r){
-        return t[v];
+    void set(int index, int value)
+    {
+        set(index, value, 0, 0, size);
     }
-    int tm=(tl+tr)/2;
-    return min(query(2*v,tl,tm,l,min(r,tm)),query(2*v+1,tm+1,tr,max(l,tm+1),r));
-}
  
+    ll query(int l, int r, int x, int lx, int rx)
+    {
+        if (lx >= r || l >= rx)
+            return INF;
+        if (lx >= l && rx <= r)
+            return table[x];
+        int m = (lx + rx) / 2;
+        ll q1 = query(l, r, 2 * x + 1, lx, m);
+        ll q2 = query(l, r, 2 * x + 2, m, rx);
+        return min(q1,q2);
+    }
+ 
+    ll query(int l, int r)
+    {
+        return query(l, r, 0, 0, size);
+    }
+};
